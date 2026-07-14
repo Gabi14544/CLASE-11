849ad5c16ac95994fad0994074a9bf3acc428725
Gabi <etirom4567@gmail.com>
2026-07-13 21:54:12 -0300

    Agrega Deposito

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from banco.gestion import crear_cuenta
-from banco.cuentas import CuentaBancaria
 
-def main ():
-    cuenta = crear_cuenta()
-    print(cuenta.titular, cuenta.saldo)
 
-main()
+
+cuenta = crear_cuenta()
+print(cuenta.titular, cuenta.saldo)
+
+while True:
+    print(f"\nCuenta de {cuenta.titular} | Saldo: ${cuenta.saldo}")
+    print("1.Depositar Dinero")
+    print("2.Retirar Dinero")
+    print("3.Salir")
+    opcion = input("Seleccione una opcion: ")
+    if opcion == "1":
+        monto_str = input("Monto a Depositar: ")
+        if monto_str.isdigit():
+            monto = int(monto_str)
+            print(cuenta.depositar(monto))
+    if opcion == "2":
+        pass
+    if opcion == "3":
+        break
